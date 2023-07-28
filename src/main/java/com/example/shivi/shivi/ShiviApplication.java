4d74f854ebfd577657210ca2d92ca534562db461
Bar0208 <shividwivedi@deloitte.com>
2023-07-28 12:07:00 +0530

    initial changes

---
+++ src/main/java/com/example/shivi/shivi/ShiviApplication.java
@@ -0,0 +1,13 @@
+package com.example.shivi.shivi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShiviApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShiviApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/shivi/shivi/ShiviApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.shivi.shivi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShiviApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
